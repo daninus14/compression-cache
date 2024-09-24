a92005e0e8322ea965a27565826459be1879782c
Daniel Nussenbaum <daninus14@gmail.com>
2024-09-24 17:59:57 +0300

    saving progress

--- compression-cache.asd
+++ compression-cache.asd
@@ -2,6 +2,6 @@
   :version "0.1.0"
   :author "Daniel Nussenbaum"
   :license "MIT"
-  :depends-on ("salza2" "clache")
+  :depends-on ("uiop" "salza2" "clache")
   :components ((:file "compression-cache"))
   :description "To provide a simple protocol for caching compressed files.")
--- compression-cache.lisp
+++ compression-cache.lisp
@@ -2,48 +2,72 @@
 
 (defpackage compression-cache
   (:use :cl)
+  (:import-from :uiop)
   (:import-from :clache)
-  (:import-from :salza2))
+  (:import-from :salza2)
+  (:export 
+   :initialize-cache
+   :ensure-path-to-compressed-file))
 
 (in-package :compression-cache)
 
 (defvar *store-path* NIL)
+(defvar *store* NIL)
 
 (defun initialize-cache (cache-path)
+  "cache-path can be either a pathname or a string of a path to a directory. If the directory doesn't exist it will be created. If a relative directory is provided AKA one not starting with a forward slash '/', the pathname will be derived from the current working directory `(uiop:getcwd)`"
   ;; check that cache-path is either a string or a pathname
-  ;; if it i's a string, turn into a pathname
+  ;; if it is a string, turn it into a pathname
   ;; then make sure the pathname points to an valid directory
   ;; if the pathname does not point to a valid directory
-  ;; signal the condition directory doesn't exist and display the directory
+  ;; signal the condition directory is not valid
   ;; if the input was not either of those types, signal a condition
-  ;; invalid input type or something (check-type)
   ;; This avoid running into issues later on, easier to catch early.
-  (uiop:ensure-pathname cache-path
-                        :want-pathname T
-                        :ensure-directory T
-                        :ensure-directories-exist T)
-  (setf *store-path* cache-path))
+  (let ((valid-cache-pathname (uiop:ensure-pathname cache-path
+                                                    :want-pathname T
+                                                    :ensure-directory T
+                                                    :ensure-directories-exist T)))
+    (setf *store-path* valid-cache-pathname)
+    (setf *store*
+          (make-instance 'file-store :directory valid-cache-pathname))))
 
-(defvar *cache-store*
-  (progn
-    (ensure-directories-exist #p"cache/")
-    (make-instance 'file-store :directory #p"cache/")))
+(defun ensure-path-to-compressed-file (filepath &optional &key (algorithm :gzip))
+  ;; TODO add check-if-modified check. Need to add the date to the value of the cache (not the key)
+  ;; and then check the modification time of the original file vs the saved value in the cache
+  ;; see https://lispcookbook.github.io/cl-cookbook/files.html#getting-file-attributes-size-access-time
+  ;; https://osicat.common-lisp.dev/
+  ;; https://github.com/Shinmera/file-attributes/
+  ;; https://shinmera.github.io/file-attributes/
+  ;; (check-if-modified NIL)
+  "Will check if there is a path for a compressed version of the original pathname and return it, if there is none, it will then compress a file and save it and return the compressed file's path."
+  (assert (uiop:file-exists-p filepath) (filepath) "File ~A not found" filepath)
+  (let* ((valid-pathname (uiop:ensure-pathname filepath
+                                               :want-pathname T))
+         (key (get-cache-key valid-pathname :algorithm algorithm))
+         (cache-value (clache:getcache key *store*)))
+    (if cache-value cache-value
+        (compress-and-cache valid-pathname :algorithm algorithm))))
 
-;; Store cache
-(setcache 1 "foo" *store*)
-;;=> 1
+(defun get-cache-key (filepath &optional &key (algorithm :gzip))
+  (clache:cache-key-to-string
+   (list :algorithm algorithm :path filepath)))
 
-;; Get cache
-(getcache 1 *store*)
-;;=> 1, T
+(defun compress-file (filepath &optional &key (algorithm :gzip))
+  (assert (equal :gzip algorithm)
+          (algorithm)
+          "Compression Algorithm ~A is not available. Currently only `:gzip` is supported"
+          algorithm)
+  (let ((compressed-filepath (uiop:merge-pathnames* filepath *store-path*)))
+    (salza2:gzip-file filepath compressed-filepath)
+    compressed-filepath))
 
-;; Get non-exited cache
-(getcache 42 *store*)
-;;=> NIL, NIL
+(defun save-to-cache (original-filepath compressed-filepath &optional &key (algorithm :gzip))
+  (clache:setcache (get-cache-key original-filepath :algorithm algorithm)
+                   compressed-filepath
+                   *store*))
 
-;; Remove cache
-(remcache 1 *store*)
-;;=> T
+(defun compress-and-cache (filepath &optional &key (algorithm :gzip))
+  (let ((compressed-filepath (compress-file filepath :algorithm algorithm)))
+    (save-to-cache filepath compressed-filepath :algorithm algorithm)
+    compressed-filepath))
 
-;; Clear all cache
-(clrcache *store*)
